ff29746573313bda023f47f75472d3b7494f26e8
jmhatch <jmhatch@vt.edu>
2024-09-10 15:23:54 -0400

    Update R data pkg

---
+++ R/data-central_atlantic_2_call_area.R
@@ -0,0 +1,11 @@
+#' @title Central Atlantic 2 Call Area
+#'
+#' @description An \code{sf} object containing the central Atlantic 2 call area.
+#'
+#' @docType data
+#' @name central_atlantic_2_call_area
+#' @usage data("central_atlantic_2_call_area")
+#' @keywords datasets
+#' @source \url{https://www.boem.gov/renewable-energy/state-activities/central-atlantic}
+NULL
+
---
+++ R/data-sea_turtle_critical_habitat.R
@@ -0,0 +1,11 @@
+#' @title Sea Turtle Critical Habitat
+#'
+#' @description An \code{sf} object containing the delineations of sea turtle critical habitat.
+#'
+#' @docType data
+#' @name sea_turtle_critical_habitat
+#' @usage data("sea_turtle_critical_habitat")
+#' @keywords datasets
+#' @source \url{https://www.fisheries.noaa.gov/national/endangered-species-conservation/critical-habitat#critical-habitat-designations-maps-and-gis-data}
+NULL
+
